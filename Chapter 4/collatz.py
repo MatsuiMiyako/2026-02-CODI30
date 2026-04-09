6a11d979ac40f1ea863efebc545a2b68133d1256
MatsuiMiyako <rowlet997@gmail.com>
2026-04-08 22:12:20 -0500

    Finished collatz practice program

---
+++ Chapter 4/collatz.py
@@ -0,0 +1,17 @@
+def collatz(number):
+    if number % 2 ==0:
+        return number // 2
+    
+    if number % 2 == 1:
+        return number * 3 + 1
+
+
+print("Enter number: ")
+currentNumber = int(input())
+numberList = ""
+
+while currentNumber != 1:
+    currentNumber = collatz(currentNumber)
+    numberList += str(currentNumber) + ' '
+
+print(numberList, sep=' ')
