36bb88dfad24fefc23ab5f48e116a7cc5abb3f2d
MatsuiMiyako <rowlet997@gmail.com>
2026-04-17 21:38:50 -0500

    Added 3 projects for Chapter 5

---
+++ Projects/Chapter 5/buggyGradeAverage.py
@@ -0,0 +1,20 @@
+def calculate_grade_average(grade_sum, number_of_grades):
+    try:
+        grade_average = int(grade_sum / number_of_grades)
+        return grade_average
+    except ZeroDivisionError:
+        grade_average = 0
+        return grade_average
+
+counter = 0
+total = 0
+while True:
+    print('Enter a grade, or "done" if done entering grades:')
+    grade = input()
+    if grade == 'done':
+        break
+    counter = counter + 1
+    total = total + int(grade)
+
+avg = calculate_grade_average(total, counter)
+print('The grade average is:', avg)
